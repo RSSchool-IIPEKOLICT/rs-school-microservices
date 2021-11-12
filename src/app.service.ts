f52a25407ff1c925c325208b42aaaa7c569e2dfa
iipekolict <iipekolict@gmail.com>
2021-11-13 00:24:30 +0300

    refactor: remove redurant data module & code restyle

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return '<h1>Hello nest</h1>'
-    }
+  getHello(): string {
+    return '<h1 style="text-align: center; color: red">ArtQuiz API worked.</h1>'
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 const port = process.env.PORT || 5000
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true })
-    await app.listen(port)
+  const app = await NestFactory.create(AppModule, { cors: true })
+  await app.listen(port)
 }
 bootstrap().then(() => console.log(`Server started on port ${port}.`))
